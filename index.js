e719aef49b56d39e15621ca366ff1a020742b347
Dominik Jungowski <Dominik@Jungowski.de>
2016-10-23 02:26:00 +0200

    adds reference to implementation base

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function(done) {
+// Implementation based on https://github.com/jasmine/jasmine-npm/issues/31#issuecomment-125953251
+
+var finishTestcase = function(done) {
 	return function(error) {
 		if (error) {
 			done.fail(error);
@@ -7,3 +9,5 @@ module.exports = function(done) {
 		}
 	}
 };
+
+module.exports = finishTestcase;
